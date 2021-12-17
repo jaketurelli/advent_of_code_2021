c57aec8d4a91d0459694d92270553c068b590760
Jake Turelli <Jake.Turelli@gmail.com>
2021-12-16 23:29:46 -0800

    day 8 part 1 complete, began part 2

--- c++/day_008.cpp
+++ c++/day_008.cpp
@@ -8,16 +8,105 @@
 /// @copyright Copyright (c) 2021
 /// 
 ///////////////////////////////////////////////
-
 #include "day_008.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
 
 size_t Solution::parse_and_anaylize(Part part)
 {
+    string line;
+    ifstream data_stream(m_data_file);
+    string delim_space = " ";
+    string delim_pipe  = "|";
+
+    vector<string> unique_output_values;
     
-    return 0;
+    while(getline(data_stream, line))
+    {
+        // 0 -> 6
+        // 1 -> 2 *
+        // 2 -> 5
+        // 3 -> 5
+        // 4 -> 4 *
+        // 5 -> 5
+        // 6 -> 6
+        // 7 -> 3 * 
+        // 8 -> 7 *
+        // 9 -> 6
+
+        // todo: logic for distinguishing segments
+        // if you know 7 and 1, you can get 'a'
+        // 
+
+
+        bool output_values_reached = false;
+        int position_next = 0;
+        int position_current = 0;
+        vector<string> input_values;
+        while(position_next!=-1)
+        {
+            
+
+            position_next = line.find(delim_space, position_current+1);
+            string curr_string = line.substr(position_current, position_next-position_current);
+            position_current=position_next+1;
+
+
+            if (curr_string.compare(delim_pipe)==0)
+                output_values_reached = true;
+            
+            if(output_values_reached)
+            {
+                if(part==Part::TWO)
+                {
+                    printf("input values: ");
+                    for (size_t i = 0; i < input_values.size(); i++)
+                    {
+                        cout << input_values[i] << ' ';
+                    }
+                    printf("\n");
+                    
+                }
+                else
+                {
+                    int curr_string_length = curr_string.length();
+                    if( curr_string_length == SEGS_PER_DIGIT[1] ||
+                        curr_string_length == SEGS_PER_DIGIT[4] ||
+                        curr_string_length == SEGS_PER_DIGIT[7] ||
+                        curr_string_length == SEGS_PER_DIGIT[8])
+                    {
+                        unique_output_values.push_back(curr_string);
+                        // cout << curr_string << ' ';
+                    }
+                }
+
+            }
+            else
+            {
+                // printf("adding: ");
+                // cout << curr_string << ' ';
+                input_values.push_back(curr_string);
+
+            }
+
+        }
+        // printf("\n");
+    }
+
+    // print unique_output_values
+    // for (size_t i = 0; i < unique_output_values.size(); i++)
+    // {
+    //     cout << unique_output_values[i] << '\n';
+    // }
+    
+    return unique_output_values.size();
 }
 
-
+size_t Solution::part1_2(Part part)
+{
+    return parse_and_anaylize(part);
+}
 int main(int argc, char const *argv[])
 {
     
@@ -25,8 +114,8 @@ int main(int argc, char const *argv[])
     // string data_file = "day_008.txt";
 
     Solution solution(data_file);
-    printf("todo: %i\n", solution.part1_2(Solution::Part::ONE));
-    printf("todo: %i\n", solution.part1_2(Solution::Part::TWO));
+    // printf("times the digits 1, 4, 7, or 8 appear: %i\n", solution.part1_2(Solution::Part::ONE));
+    printf("What you get if you add up all of the output values: %i\n", solution.part1_2(Solution::Part::TWO));
 
     return 0;
 }
--- c++/day_008.h
+++ c++/day_008.h
@@ -1,6 +1,7 @@
 #ifndef DAY_008_H_
 #define DAY_008_H_
 #include <string>
+#include <map>
 using namespace std;
 
 class Solution
@@ -14,6 +15,18 @@ public:
 private:
     string m_data_file;
     size_t parse_and_anaylize(Part part);
+
+    static constexpr int SEGS_PER_DIGIT[10]={
+        6,  // 0
+        2,  // 1
+        5,  // 2
+        5,  // 3
+        4,  // 4
+        5,  // 5
+        6,  // 6
+        3,  // 7
+        7,  // 8
+        6}; // 9
 
 public:
     Solution(string data_file):
