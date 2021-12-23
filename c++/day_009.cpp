fd54446cc656fe40a4e0257cb50878fdefdd0762
Jake Turelli <Jake.Turelli@gmail.com>
2021-12-23 00:02:00 -0800

    (mostly) completed day 9 part 2. still should sort the final basin values and select top three.

--- c++/day_009.cpp
+++ c++/day_009.cpp
@@ -4,16 +4,15 @@
 /// @brief https://adventofcode.com/2021/day/9
 /// @version 0.1
 /// @date 2021-12-17
-/// 
+///
 /// @copyright Copyright (c) 2021
-/// 
+///
 ///////////////////////////////////////////////
 #include "day_009.h"
 #include <fstream>
 #include <iostream>
 
-
-void Solution::fill_array(string& line, uint8_t* array, size_t size_of_array)
+void Solution::fill_array(string &line, uint8_t *array, size_t size_of_array)
 {
     for (size_t i = 0; i < size_of_array; i++)
     {
@@ -23,7 +22,7 @@ void Solution::fill_array(string& line, uint8_t* array, size_t size_of_array)
 
 void Solution::add_low_point(uint8_t value, int row, int col)
 {
-    m_low_points.push_back(LowPoint(value, row, col));
+    m_low_points.push_back(LowPoint(row, col, value));
     // printf("low point: %1i (%3i,%3i)\n", value, row, col);
 }
 
@@ -34,30 +33,29 @@ size_t Solution::parse_and_analyze()
     string line_next;
     ifstream data_stream(m_data_file);
     int line_number = 0;
-    
 
     // determine number of digits per line
-    
+
     getline(data_stream, line_current);
     const int N_DATA = line_current.length();
-    m_n_cols = N_DATA;
+    // m_n_cols = N_DATA;
     // printf("N_DATA: %i\n", N_DATA);
 
     uint8_t data_0[N_DATA] = {0};
     uint8_t data_1[N_DATA] = {0};
     uint8_t data_2[N_DATA] = {0};
 
-    uint8_t* const pointer_bookie[3] = { &data_0[0],
-                                         &data_1[0],
-                                         &data_2[0]};
+    uint8_t *const pointer_bookie[3] = {&data_0[0],
+                                        &data_1[0],
+                                        &data_2[0]};
 
-    uint8_t* data_previous_p = nullptr;
-    uint8_t* data_current_p  = pointer_bookie[0];
-    uint8_t* data_next_p =     pointer_bookie[1];
+    uint8_t *data_previous_p = nullptr;
+    uint8_t *data_current_p = pointer_bookie[0];
+    uint8_t *data_next_p = pointer_bookie[1];
 
-    fill_array(line_current, data_current_p,N_DATA);
+    fill_array(line_current, data_current_p, N_DATA);
     getline(data_stream, line_next);
-    fill_array(line_next, data_next_p,N_DATA);
+    fill_array(line_next, data_next_p, N_DATA);
 
     int current_line_number = 0;
 
@@ -65,174 +63,215 @@ size_t Solution::parse_and_analyze()
     for (size_t i = 0; i < N_DATA; i++)
     {
         // printf("%i ", data_current_p[i]);
-        if( i==0 && 
-            data_current_p[i] < data_current_p[i+1] &&
-            data_current_p[i] < data_next_p[i] )
+        if (i == 0 &&
+            data_current_p[i] < data_current_p[i + 1] &&
+            data_current_p[i] < data_next_p[i])
         {
             add_low_point(data_current_p[i], current_line_number, i);
         }
 
-        else if( i==(N_DATA-1) && 
-                    data_current_p[i] < data_current_p[i-1] &&
-                    data_current_p[i] < data_next_p[i])
-        {
-            add_low_point(data_current_p[i], current_line_number, i);
-        }
-
-        else if( data_current_p[i] < data_current_p[i+1] && 
-                 data_current_p[i] < data_current_p[i-1] && 
+        else if (i == (N_DATA - 1) &&
+                 data_current_p[i] < data_current_p[i - 1] &&
                  data_current_p[i] < data_next_p[i])
         {
             add_low_point(data_current_p[i], current_line_number, i);
         }
 
-        
+        else if (data_current_p[i] < data_current_p[i + 1] &&
+                 data_current_p[i] < data_current_p[i - 1] &&
+                 data_current_p[i] < data_next_p[i])
+        {
+            add_low_point(data_current_p[i], current_line_number, i);
+        }
     }
     // printf("\n");
 
     current_line_number++;
-    while(getline(data_stream, line_next))
+    while (getline(data_stream, line_next))
     {
-        
-        data_previous_p = pointer_bookie[(current_line_number-1)%3];
-        data_current_p  = pointer_bookie[current_line_number%3];
-        data_next_p     = pointer_bookie[(current_line_number+1)%3];
 
+        data_previous_p = pointer_bookie[(current_line_number - 1) % 3];
+        data_current_p = pointer_bookie[current_line_number % 3];
+        data_next_p = pointer_bookie[(current_line_number + 1) % 3];
 
-        fill_array(line_next,data_next_p,N_DATA);
-        
+        fill_array(line_next, data_next_p, N_DATA);
 
         for (size_t i = 0; i < N_DATA; i++)
         {
             // printf("%i ", data_current_p[i]);
-            if( i==0 )
+            if (i == 0)
             {
                 // printf("%i vs (%i, %i, %i) ", data_current_p[i], data_current_p[i+1], data_next_p[i], data_previous_p[i]);
-                if( data_current_p[i] < data_current_p[i+1] &&
-                    data_current_p[i] < data_next_p[i] && 
+                if (data_current_p[i] < data_current_p[i + 1] &&
+                    data_current_p[i] < data_next_p[i] &&
                     data_current_p[i] < data_previous_p[i])
                 {
                     add_low_point(data_current_p[i], current_line_number, i);
                 }
             }
-            else if( i==(N_DATA-1))
+            else if (i == (N_DATA - 1))
             {
                 // printf("%i vs (%i, %i, %i) ", data_current_p[i], data_current_p[i-1], data_next_p[i], data_previous_p[i]);
-                if(  data_current_p[i] < data_current_p[i-1] &&
-                     data_current_p[i] < data_next_p[i] && 
-                     data_current_p[i] < data_previous_p[i])
+                if (data_current_p[i] < data_current_p[i - 1] &&
+                    data_current_p[i] < data_next_p[i] &&
+                    data_current_p[i] < data_previous_p[i])
                 {
 
                     add_low_point(data_current_p[i], current_line_number, i);
                 }
             }
 
-            else 
+            else
             {
                 // printf("%i ", data_current_p[i]);
                 // printf("%i vs (%i, %i, %i, %i) ", data_current_p[i], data_current_p[i+1], data_current_p[i-1], data_next_p[i], data_previous_p[i]);
-                if( data_current_p[i] < data_current_p[i+1] && 
-                     data_current_p[i] < data_current_p[i-1] && 
-                     data_current_p[i] < data_next_p[i] && 
-                     data_current_p[i] < data_previous_p[i])
+                if (data_current_p[i] < data_current_p[i + 1] &&
+                    data_current_p[i] < data_current_p[i - 1] &&
+                    data_current_p[i] < data_next_p[i] &&
+                    data_current_p[i] < data_previous_p[i])
                 {
                     add_low_point(data_current_p[i], current_line_number, i);
                 }
             }
-            
         }
-        
+
         current_line_number++;
         // printf("\n");
     }
-    m_n_rows = current_line_number+1;
+    // m_n_rows = current_line_number + 1;
 
-    data_previous_p = pointer_bookie[(current_line_number-1)%3];
-    data_current_p  = pointer_bookie[current_line_number%3];
+    data_previous_p = pointer_bookie[(current_line_number - 1) % 3];
+    data_current_p = pointer_bookie[current_line_number % 3];
 
     // somethign different for first line
     for (size_t i = 0; i < N_DATA; i++)
     {
         // printf("%i ", data_current_p[i]);
-        if( i==0 && 
-            data_current_p[i] < data_current_p[i+1] &&
-            data_current_p[i] < data_previous_p[i] )
+        if (i == 0 &&
+            data_current_p[i] < data_current_p[i + 1] &&
+            data_current_p[i] < data_previous_p[i])
         {
             add_low_point(data_current_p[i], current_line_number, i);
         }
 
-        else if( i==(N_DATA-1) && 
-                    data_current_p[i] < data_current_p[i-1] &&
-                    data_current_p[i] < data_previous_p[i])
-        {
-            add_low_point(data_current_p[i], current_line_number, i);
-        }
-
-        else if( data_current_p[i] < data_current_p[i+1] && 
-                 data_current_p[i] < data_current_p[i-1] && 
+        else if (i == (N_DATA - 1) &&
+                 data_current_p[i] < data_current_p[i - 1] &&
                  data_current_p[i] < data_previous_p[i])
         {
             add_low_point(data_current_p[i], current_line_number, i);
         }
 
-        
+        else if (data_current_p[i] < data_current_p[i + 1] &&
+                 data_current_p[i] < data_current_p[i - 1] &&
+                 data_current_p[i] < data_previous_p[i])
+        {
+            add_low_point(data_current_p[i], current_line_number, i);
+        }
     }
     // printf("\n");
 
-    printf("Low points: \n");
+    // printf("Low points: \n");
     int risk_level_total = 0;
     for (auto i = m_low_points.begin(); i != m_low_points.end(); i++)
     {
-        risk_level_total += i->val+1;
-        printf("%i (%3i,%3i)\n",i->val,i->row,i->col);
+        risk_level_total += i->val + 1;
+        // printf("%i (%3i,%3i)\n", i->val, i->row, i->col);
     }
-    printf("\n");
+    // printf("\n");
     return risk_level_total;
-    
 }
 
-size_t Solution::get_basin_value(uint8_t row, uint8_t col, uint8_t val);
+size_t Solution::get_basin_points(LowPoint &low_point, uint8_t row, uint8_t col, uint8_t from_val, FromDirection from_dir)
 {
-    start here
-    if(get_basin_value())
-}
+    // start point
+    size_t basin_value = 0;
+    uint8_t curr_val = m_data_matrix[row][col];
+    if (from_dir == FromDirection::StartPoint)
+    {
+        // printf("adding basin point (%3i,%3i)(%i)\n", row, col, from_val);
+        low_point.basin_points.push_back(BasinPoint(row, col, from_val));
+        basin_value += from_val;
+    }
+    else
+    {
+        // check to make sure we don't duplicate points
+        for (auto i = low_point.basin_points.begin(); i != low_point.basin_points.end(); i++)
+        {
+            if (i->row == row && i->col == col)
+                return 0;
+        }
 
+        // check for general basin requirement
+        if (from_val > curr_val || curr_val == 9)
+            return basin_value;
+        else
+        {
+            // printf("adding basin point (%3i,%3i)(%i)\n", row, col, curr_val);
+            low_point.basin_points.push_back(BasinPoint(row, col, curr_val));
+            basin_value += curr_val;
+        }
+    }
+
+    if (from_dir != FromDirection::FromHigh)
+    {
+        if (row != 0)
+            basin_value += get_basin_points(low_point, row - 1, col, curr_val, FromDirection::FromLow);
+    }
+
+    if (from_dir != FromDirection::FromLow)
+    {
+        if (row != m_n_rows - 1)
+            basin_value += get_basin_points(low_point, row + 1, col, curr_val, FromDirection::FromHigh);
+    }
+
+    if (from_dir != FromDirection::FromLeft)
+    {
+        if (col != 0)
+            basin_value += get_basin_points(low_point, row, col - 1, curr_val, FromDirection::FromRight);
+    }
+
+    if (from_dir != FromDirection::FromRight)
+    {
+        if (col != m_n_cols - 1)
+            basin_value += get_basin_points(low_point, row, col + 1, curr_val, FromDirection::FromLeft);
+    }
+
+    return basin_value;
+}
 
 void Solution::parse_and_fill_matrix()
 {
-    printf("hi?\n");
     string line;
     ifstream data_stream(m_data_file);
-    printf("creating matrix [%llu][%llu]\n",m_n_rows,m_n_cols);
-    uint8_t data_matrix[m_n_rows][m_n_cols]={0};
     size_t curr_row = 0;
-    printf("writing data to matrix.\n");
-    while(getline(data_stream, line))
+    vector<size_t> basin_sizes;
+    while (getline(data_stream, line))
     {
         for (size_t i = 0; i < line.length(); i++)
         {
-            uint8_t val = line[i]-'0';
+            uint8_t val = line[i] - '0';
             // printf("%i", val);
-            data_matrix[curr_row][i] = val;
+            m_data_matrix[curr_row][i] = val;
         }
         // printf(" (%i)\n",curr_row);
         curr_row++;
-
     }
 
     // recursively find paths
     for (auto i = m_low_points.begin(); i != m_low_points.end(); i++)
     {
-        size_t basin_value = get_basin_value(i->row, i->col, i->val);
+        size_t basin_value = get_basin_points(*i, i->row, i->col, i->val, FromDirection::StartPoint);
+        // printf("basin value (%3i,%3i (%3i)(size:%5i)): %llu\n", i->row, i->col, i->val, i->basin_points.size(), basin_value);
+        printf("basin size:%5i\n", i->basin_points.size());
     }
     printf("\n");
-    
+
     printf("done writing data to matrix.\n");
 }
 
 size_t Solution::solve()
 {
-    size_t number_low_points =  parse_and_analyze();
+    size_t number_low_points = parse_and_analyze();
     parse_and_fill_matrix();
     return number_low_points;
 }
--- c++/day_009.h
+++ c++/day_009.h
@@ -9,31 +9,52 @@ using namespace std;
 class Solution
 {
 private:
-    struct LowPoint
+    enum FromDirection
     {
-        uint8_t val;
+        FromLow,
+        FromHigh,
+        FromRight,
+        FromLeft,
+        StartPoint
+    };
+
+    struct BasinPoint
+    {
+
         uint8_t row;
         uint8_t col;
-        LowPoint(uint8_t _val, uint8_t _row, uint8_t _col):
-            val(_val),
-            row(_row),
-            col(_col){};
+        uint8_t val;
+        BasinPoint(uint8_t _row, uint8_t _col, uint8_t _val) : row(_row),
+                                                               col(_col),
+                                                               val(_val){};
+    };
+    struct LowPoint
+    {
+
+        uint8_t row;
+        uint8_t col;
+        uint8_t val;
+        vector<BasinPoint> basin_points;
+        LowPoint(uint8_t _row, uint8_t _col, uint8_t _val) : row(_row),
+                                                             col(_col),
+                                                             val(_val){};
     };
     string m_data_file;
-    size_t m_n_cols;
-    size_t m_n_rows;
+    static const size_t m_n_cols = 100; // 10
+    static const size_t m_n_rows = 100; // 5
+    uint8_t m_data_matrix[m_n_rows][m_n_cols];
     vector<LowPoint> m_low_points;
-    void fill_array(string& line, uint8_t array[], size_t size_of_array);
+    void fill_array(string &line, uint8_t array[], size_t size_of_array);
     void add_low_point(uint8_t value, int row, int col);
+    size_t get_basin_points(LowPoint &low_point, uint8_t row, uint8_t col, uint8_t val, FromDirection from_dir);
     void parse_and_fill_matrix();
     size_t parse_and_analyze();
+
 public:
-    Solution(string data_file):
-        m_data_file(data_file),m_n_rows(0){};
+    Solution(string data_file) : m_data_file(data_file){};
     ~Solution(){};
 
     size_t solve();
 };
-
 
 #endif // DAY_009_H_
