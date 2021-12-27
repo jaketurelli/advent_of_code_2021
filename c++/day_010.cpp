b2fcd9d7ccac8659157f97ab166a486a6b4af84d
Jake Turelli <Jake.Turelli@gmail.com>
2021-12-26 22:05:02 -0800

    completed day 10 part 2.

--- c++/day_010.cpp
+++ c++/day_010.cpp
@@ -16,7 +16,7 @@
 
 //todo: need to do more of a push/pop child/parent solution
 
-int Solution::part1()
+Solution::Answer Solution::get_answer()
 {
     string line;
     ifstream data_stream(m_data_file);
@@ -24,7 +24,10 @@ int Solution::part1()
 
     while (getline(data_stream, line))
     {
+        size_t incomplete_score = 0; // part 2;
+
         stack<char> m_part1_stack;
+        bool line_is_corrupt = false;
 
         for (size_t i = 0; i < line.length(); i++)
         {
@@ -33,35 +36,77 @@ int Solution::part1()
             if (cur_char == '(' || cur_char == '[' || cur_char == '{' || cur_char == '<')
 
             {
-                // openers[cur_char]++;
                 m_part1_stack.push(cur_char);
-                // printf("openers[%c]: %i\n", cur_char, openers[cur_char]);
+                // printf("adding %c\n", cur_char);
             }
             else
             {
-                // closers[cur_char]++;
                 if (m_part1_stack.top() == cur_char_pair)
+                {
+                    // printf("popping %c\n", cur_char_pair);
                     m_part1_stack.pop();
+                }
                 else
                 {
-                    cout << line << " | ";
-                    printf("syntax error found! (%c expected, found %c)\n", m_pairs[m_part1_stack.top()], cur_char);
+                    // cout << line << " | ";
+                    // printf("syntax error found! (%c expected, found %c)\n", m_pairs[m_part1_stack.top()], cur_char);
                     score += m_points[cur_char];
+                    line_is_corrupt = true;
                     break;
                 }
             }
         }
-    }
 
-    return score;
+        // handle non-corrupt lines for part 2
+        if (!line_is_corrupt)
+        {
+            if (m_part1_stack.size() > 0)
+            {
+                // printf("characters left: %i\n", m_part1_stack.size());
+
+                for (size_t i = 0; i < m_part1_stack.size();)
+                {
+                    printf("%c", m_pairs[m_part1_stack.top()]);
+                    incomplete_score = 5 * incomplete_score + m_incomplete_points[m_pairs[m_part1_stack.top()]];
+                    // printf("%c", m_part1_stack.top());
+                    m_part1_stack.pop();
+                }
+
+                m_incomplete_scores.push(incomplete_score);
+                printf(" | %llu\n", incomplete_score);
+            }
+            else
+            {
+                printf("this line is not incomplete\n");
+            }
+        }
+    }
+    m_answer.part1 = score;
+
+    size_t number_incomplete = m_incomplete_scores.size();
+    size_t index = (number_incomplete / 2) + 1;
+    for (size_t i = 0; i < number_incomplete; i++)
+    {
+        if (i == index - 1)
+        {
+            m_answer.part2 = m_incomplete_scores.top();
+            return m_answer;
+        }
+        else
+            m_incomplete_scores.pop();
+    }
+    return m_answer;
 }
 
 int main(int argc, char const *argv[])
 {
-    string data_file = "day_010_example.txt";
+    // string data_file = "day_010_example.txt";
+    string data_file = "day_010.txt";
     Solution solution(data_file);
 
-    int points = solution.part1();
-    printf("points: %i\n", points);
+    Solution::Answer answer = solution.get_answer();
+    printf("points: %i\n", answer.part1);
+    printf("score : %llu\n", answer.part2);
+
     return 0;
 }
--- c++/day_010.h
+++ c++/day_010.h
@@ -2,6 +2,7 @@
 #define DAY_010_H_
 #include <string>
 #include <map>
+#include <queue>
 using namespace std;
 class Solution
 {
@@ -25,11 +26,29 @@ private:
         {'}', 1197},
         {'>', 25137}};
 
+    map<char, int> m_incomplete_points = {
+        {')', 1},
+        {']', 2},
+        {'}', 3},
+        {'>', 4}};
+
+    priority_queue<size_t> m_incomplete_scores;
+
 public:
     Solution(string data_file) : m_data_file(data_file){};
     ~Solution(){};
 
-    int part1();
+    struct Answer
+    {
+        int part1{0};
+        int part2{0};
+    };
+
+private:
+    Answer m_answer;
+
+public:
+    Answer get_answer();
     int part2();
 };
 
